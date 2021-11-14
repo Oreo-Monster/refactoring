c7a18edc50c202de66cf70856577d0a7f6bacb12
nbeck415 <natalielunbeck@yahoo.com>
2021-11-14 17:23:20 -0500

    add getters and setters to grid

--- after/grid.py
+++ after/grid.py
@@ -6,8 +6,11 @@ class Grid:
         self.offset = offset
         self.tile_size = tile_size
 
-    def get(self, coordinates):
+    def getTileValue(self, coordinates):
         return self.grid[coordinates['row'], coordinates['col']]
+    
+    def setTileValue(self, coordinates, value):
+        self.grid[coordinates['row'], coordinates['col']] = value
 
     def getTileSize(self):
         return self.tile_size
@@ -17,3 +20,23 @@ class Grid:
 
     def getShape(self):
         return self.grid.shape
+
+    def getCol(self, col):
+        return self.grid[:, col]
+    
+    def getRow(self, row):
+        return self.grid[row, :]
+    
+    def getDiagonal(self):
+        #return every diagonal in the array
+        diagonals = []
+        for i in range(-(self.grid.shape[0]-1),self.grid.shape[1]):
+            diagonals.append(self.grid.diagonal(offset=i))
+            diagonals.append(np.fliplr(self.grid).diagonal(offset=i))
+        return diagonals
+    
+    def getIndices(self, pos):
+        indices = {}
+        indices['col'] = int(abs((pos['x'] - self.offset['x'] - (self.tile_size/2)) // (self.tile_size) + 1))
+        indices['row'] = int(abs((pos['y'] - self.offset['y'] - (self.tile_size/2)) // (self.tile_size) + 1))
+        return indices
