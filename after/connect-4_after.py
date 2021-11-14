5f9a06251e0715f73d42e84950ddf37bdc1587ae
Oreo-Monster <ewrong16@gmail.com>
2021-11-14 16:06:02 -0500

    Moved window functionality to GameWindow Class

--- after/connect-4_after.py
+++ after/connect-4_after.py
@@ -7,54 +7,9 @@ description:
 '''
 
 import turtle
+from window import GameWindow
 
 
-def make_window(window_title, bgcolor, width, height):
-	''' this function creates a screen object and returns it '''
-
-	window = turtle.getscreen() # Set the window size
-	window.title(window_title)
-	window.bgcolor(bgcolor)
-	window.setup(width, height)
-	window.tracer(0) #turns off screen updates for the window Speeds up the game
-	return window
-
-
-def make_turtle(shape, color, stretch_width, stretch_length, x_pos, y_pos):
-    ''' creates a turtle and sets initial position '''
-
-    turt = turtle.Turtle()
-    turt.speed(0)    # Speed of animation, 0 is max
-    turt.shape(shape)
-    turt.color(color)
-    turt.shapesize(stretch_width, stretch_length) 
-    turt.penup()
-    turt.goto(x_pos, y_pos) # Start position
-    return turt
-
-
-def draw_grid(grid, turt, x_pos, y_pos, tile_size):
-    ''' draws a grid at x, y with a specific tile_size '''
-
-    turt.up()
-    turt.goto(x_pos, y_pos)
-    turt.down()
-
-    for row in range(len(grid)):
-        for col in range(len(grid[row])):
-            
-            turt.up()
-            turt.goto(x_pos + col * tile_size, y_pos -row * tile_size)
-            turt.down()
-
-            if grid[row][col] == 1:
-                turt.dot(tile_size-5, "red")
-            
-            elif grid[row][col] == 2:
-                turt.dot(tile_size-5, "yellow")
-            
-            else:
-                turt.dot(tile_size-5, "white")
 
 
 def check_win(grid, player):
@@ -104,7 +59,7 @@ def check_win(grid, player):
 
 
 # setting up the window
-window = make_window("Connect 4", "light sky blue", 800, 600)
+window = make_window("Connect 4", "light sky blue", 800, 600) #No global code
 
 
 # the grid
@@ -125,6 +80,7 @@ turn = 1
 def play(x_pos, y_pos):
     ''' '''
     global turn
+
     row = int(abs((y_pos - y_offset - 25) // (50) + 1))
     col = int(abs((x_pos - x_offset - 25) // (50) + 1))
     print(row, col)
@@ -132,7 +88,7 @@ def play(x_pos, y_pos):
     draw_grid(grid, my_turtle, x_offset, y_offset, tile_size)
     window.update()
 
-    if check_win(grid, 1):
+    if check_win(grid, 1): #Only need to check 
         print("player 1 won")
 
     elif check_win(grid, 2):
@@ -144,8 +100,7 @@ def play(x_pos, y_pos):
         turn = 1
 
 
-window.onscreenclick(play)
-window.listen()
+
 
 def main():
     ''' the main function where the game events take place '''
@@ -154,36 +109,37 @@ def main():
 
     draw_grid(grid, my_turtle, x_offset, y_offset, tile_size)
 
-    while True:
+    while True: #Make some exit condition
 
-        # grid[1][0] = 1
-        # grid[2][1] = 1
-        # grid[3][2] = 1
-        # grid[4][3] = 1
+        # selected_row = int(input("enter row, player "+ str(turn) +": "))
+        # selected_col = int(input("enter col, player "+ str(turn) +": "))
 
-        selected_row = int(input("enter row, player "+ str(turn) +": "))
-        selected_col = int(input("enter col, player "+ str(turn) +": "))
+        # if grid[selected_row][selected_col] == 0:
 
-        if grid[selected_row][selected_col] == 0:
-
-            if turn == 1:
-                grid[selected_row][selected_col] = 1
-            else:
-                grid[selected_row][selected_col] = 2
+        #     if turn == 1:
+        #         grid[selected_row][selected_col] = 1
+        #     else:
+        #         grid[selected_row][selected_col] = 2
 
         draw_grid(grid, my_turtle, -150, 200, 50)
         window.update()
 
-        if check_win(grid, 1):
-            print("player 1 won")
+        # if check_win(grid, 1):
+        #     print("player 1 won")
 
-        elif check_win(grid, 2):
-            print("player 2 won")
+        # elif check_win(grid, 2):
+        #     print("player 2 won")
 
-        if turn == 1:
-            turn = 2
-        else:
-            turn = 1
+        # if turn == 1:
+        #     turn = 2
+        # else:
+        #     turn = 1
+
+        window.onscreenclick(play)
+        window.listen()
+
+        # window.onclick(play())
+        # play()
 
 
     # window.exitonclick()
---
+++ after/window.py
@@ -0,0 +1,62 @@
+import turtle
+
+class GameWindow():
+    
+    def __init__(self, on_click, grid,name="Connect 4", color="light sky blue", shape={'width':800, 'height':600}):
+        self.name = name
+        self.color = color
+        self.shape = shape
+        self.window = turtle.Screen()
+        self.window.title(name)
+        self.window.bgcolor(color)
+        self.window.setup(shape['width'], shape['height'])
+        self.window.tracer(0)
+        self.window.onclick(on_click)
+        self.turt = None
+        self.grid = grid
+
+    def update(self):
+        self.window.update()
+    
+    def listen(self):
+        self.window.listen()
+
+    def close(self):
+        self.window.close()
+
+    def make_turtle(self, shape, color, stretch, pos):
+        ''' creates a turtle and sets initial position '''
+
+        turt = turtle.Turtle()
+        turt.speed(0)    # Speed of animation, 0 is max
+        turt.shape(shape)
+        turt.color(color)
+        turt.shapesize(stretch['width'], stretch['length']) 
+        turt.penup()
+        turt.goto(pos['x'], pos['y']) # Start position
+        self.turt = turt
+
+    def goto(self, pos):
+        ''' moves the turtle to x, y '''
+        self.turt.up()
+        self.turt.goto(pos['x'], pos['y'])
+        self.turt.down()
+
+    def draw_grid(self):
+        ''' draws a grid at x, y with a specific tile_size '''
+        
+        offsets = self.grid.getOffsets()
+        tileSize = self.grid.getTileSize()
+        shape = self.grid.getShape()
+        colors = {1: 'red', 2: 'yellow', 0: 'white'}
+        self.goto(offsets)
+
+        for row in range(shape['row']):
+            for col in range(shape['col']):
+                self.turtle.goto(offsets['x']+ col * tileSize, offsets['y']+row * tileSize)
+                self.turt.dot(tileSize-5, colors[self.grid.get(row, col)])
+                
+
+        
+
+        
