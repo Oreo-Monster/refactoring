1dfe4607a4ed40fb324671c4ad8bdbe08d258e6c
nbeck415 <natalielunbeck@yahoo.com>
2021-11-14 16:26:00 -0500

    add class def to Grid

--- after/grid.py
+++ after/grid.py
@@ -1,18 +1,19 @@
 import turtle, numpy as np
 
-def __init__(self, shape={'row':5, 'col':7}, offset={'x':-150, 'y':200}, tile_size=50):
-    self.grid = np.zeros(shape['row'], shape['col'])
-    self.offset = offset
-    self.tile_size = tile_size
+class Grid:
+    def __init__(self, shape={'row':5, 'col':7}, offset={'x':-150, 'y':200}, tile_size=50):
+        self.grid = np.zeros(shape['row'], shape['col'])
+        self.offset = offset
+        self.tile_size = tile_size
 
-def get(self, coordinates):
-    return self.grid[coordinates['row'], coordinates['col']]
+    def get(self, coordinates):
+        return self.grid[coordinates['row'], coordinates['col']]
 
-def getTileSize(self):
-    return self.tile_size
+    def getTileSize(self):
+        return self.tile_size
 
-def getOffsets(self):
-    return self.offset
+    def getOffsets(self):
+        return self.offset
 
-def getShape(self):
-    return self.grid.shape
+    def getShape(self):
+        return self.grid.shape
