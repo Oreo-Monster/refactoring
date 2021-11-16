d9e1b00902a7a8c9ec6001bfdd812e982f1f493e
Oreo-Monster <ewrong16@gmail.com>
2021-11-15 21:19:00 -0500

    Cleaned up paddle

--- after/paddle.py
+++ after/paddle.py
@@ -3,35 +3,21 @@ import turtle
 class Paddle:
     # implements a Pong game paddle
 
-    def __init__(self, pos):
+    def __init__(self, turtle, pos, vel=20):
         # store pos as a dictionary
         ''' initializes a paddle with a position '''
-
         self.pos = pos
+        self.turt = turtle
+        self.vel = vel
 
-        self.turt = make_turtle("square", "white", 5, 1, self.x_position, self.y_position)
+    def move(self, direction):
+        ''' moves the paddle in the direction specified '''
+        if direction == 'up':
+            self.pos['y'] += self.vel
+        elif direction == 'down':
+            self.pos['y'] -= self.vel
+        self.turt.sety(self.pos['y'])
 
-
-    def up(self):
-        y = self.turt.ycor()
-        y += 20
-        self.turt.sety(y)
-        self.y_position = y
-
-
-    def down(self):
-        y = self.turt.ycor() #Get the current y coordinate
-        y -= 20             #add 20px could also be y=y+20
-        self.turt.sety(y)    #move the paddle to the new y position
-        self.y_position = y
-
-
-    def xcor(self):
-        ''' returns turtle x_cord '''
-        return self.turt.xcor()
-
-    
-    def ycor(self):
-        ''' returns turtle y_cord '''
-        return self.turt.ycor()
+    def getPos(self):
+        return self.pos
 
