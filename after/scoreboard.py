fc2a651b79995c90c6694db199758460fba22a75
Oreo-Monster <ewrong16@gmail.com>
2021-11-15 21:42:00 -0500

    Added Scoreboard class to store and draw score

---
+++ after/scoreboard.py
@@ -0,0 +1,14 @@
+
+
+class Scoreboard:
+
+    def __init__(self, turtle, score={'p1':0, 'p2':0}):
+        self.turt = turtle
+        self.score = score
+
+    def draw(self):
+        self.turt.write(f"Player A: {self.score['p1']}  Player B: {self.score['p2']}", align="center", font=("Courier", 24, "normal"))
+        self.turt.hideturtle()
+
+    def increment(self, player):
+        self.score[player] += 1
